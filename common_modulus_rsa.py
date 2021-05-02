7f8e2861f64af12e07ce8386a689b1eb230e9d5e
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2021-05-02 14:22:23 -0700

    rsa common modulus

---
+++ common_modulus_rsa.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.4
+import gmpy2
+import argparse
+from math import gcd
+
+parser = argparse.ArgumentParser(description='RSA Common modulus attack')
+required_named = parser.add_argument_group('required named arguments')
+required_named.add_argument('-n', '--modulus', help='Common modulus', type=int, required=True)
+required_named.add_argument('-e1', '--key1', help='First public key', type=int, required=True)
+required_named.add_argument('-e2', '--key2', help='Second public key', type=int, required=True)
+required_named.add_argument('-c1', '--ciphertext1', help='First ciphertext', type=int, required=True)
+required_named.add_argument('-c2', '--ciphertext2', help='Second ciphertext', type=int, required=True)
+
+
+def common_modulus_attack(c1, c2, e1, e2, n):
+    if gcd(e1, e2) != 1:
+        raise ValueError("Common modulus attack requires gcd(e1,e2) = 1.")
+        
+    a = gmpy2.invert(e1, e2)
+    b = float(1 - (a * e1)) / e2
+
+    i = gmpy2.invert(c2, n)
+    mx = pow(c1, a, n)
+    my = pow(i, int(-b), n)
+    return mx * my % n
+
+def main():
+    args = parser.parse_args()
+    print('Starting attack...')
+
+    try:
+        result = hex(common_modulus_attack(args.ciphertext1, args.ciphertext2, args.key1, args.key2, args.modulus))[2:]
+        print('Success!')
+        print(bytes.fromhex(result).decode("ASCII"))
+    except Exception as e:
+        print('Attack failed!')
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
